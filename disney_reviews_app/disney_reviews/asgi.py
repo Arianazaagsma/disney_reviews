7822f7d96ac1ec79615bb10471143e0052ac26a5
Ariana Zaagsma <arianazaagsma@gmail.com>
2022-07-16 10:27:10 -0700

    changed all nc_tutorials references

--- disney_reviews_app/disney_reviews/asgi.py
+++ disney_reviews_app/disney_reviews/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for nc_tutorials project.
+ASGI config for disney_reviews project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nc_tutorials.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disney_reviews.settings')
 
 application = get_asgi_application()
