a3eaa69f2336f3bdb1390fdf53e39cab1c9d9208
Ariana Zaagsma <arianazaagsma@gmail.com>
2022-07-22 13:31:38 -0700

    updated models for reviews and users

--- disney_reviews_app/reviews/models.py
+++ disney_reviews_app/reviews/models.py
@@ -1,1 +1,20 @@
 # models for tables
+
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Shows_Movies(models.Model):
+    title = models.CharField(max_length=280)
+    type = models.CharField(max_length=6)
+    description = models.CharField()
+    release_year = models.DateField()
+    age_certification = models.CharField()
+    runtime = models.IntegerField()
+    genre = models.CharField()
+    seasons = models.IntegerField
+
+
+class Reviews(models.Model):
+    rating = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    review = models.CharField(max_length=280)
+    show_movie_id = models.ForeignKey(Shows_Movies, on_delete=models.CASCADE)
--- disney_reviews_app/users/models.py
+++ disney_reviews_app/users/models.py
@@ -1,1 +1,16 @@
 # models for tables
+
+from tkinter import CASCADE
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from reviews.models import Reviews
+
+
+class Users(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=280)
+
+
+class Users_Reviews(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=CASCADE)
+    review_id = models.ForeignKey(Reviews, on_delete=CASCADE)
